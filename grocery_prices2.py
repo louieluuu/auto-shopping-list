9004f2004dda4075661195c72bb44b3515409ebd
Louie <yi-jie.lu@hotmail.com>
2022-09-23 14:55:31 -0700

    Add No Frills compatibility
    Mostly copy-paste from Superstore, but the badges are
    slightly different on No Frills.

--- grocery_prices2.py
+++ grocery_prices2.py
@@ -51,34 +51,34 @@ tnt_url = "https://www.tntsupermarket.com/eng/75551501-tnt-thai-jasmine-rice-40l
 
 
 
+
 new_tab = "chrome://newtab"
 superstore_url = "https://www.realcanadiansuperstore.ca/"
 
-
-# instantiating Superstore foods
-class Food:
+# instantiating Superstore items
+class Item:
     def __init__(self, search, desired_item, max_buy_price, cheapest_price):
         self.search = search
         self.desired_item = desired_item
         self.max_buy_price = max_buy_price
         self.cheapest_price = cheapest_price
 
-lentils = Food("red split lentils", "1.8 kg", 4.50, 3.99)
-peas = Food("no name green peas", "Club Size", 5.00, 4.49)
-oats = Food("quick oats", "5.16 kg", 10.00, 6.49)
-peanut_butter = Food("no name peanut butter", "Club Size", 8.00, 6.99)
-condensed_milk = Food("condensed milk", "Brand Sweetened Condensed Milk", 3.00, 2.49)
-chicken_korma = Food("chicken korma", "President's Choice", 4.00, 3.49)
-annies_crackers = Food("annies crackers", "White Cheddar", 4.00, 3.49)
+lentils = Item("red split lentils", "1.8 kg", 4.50, 3.99)
+peas = Item("no name green peas", "Club Size", 5.00, 4.49)
+oats = Item("quick oats", "5.16 kg", 10.00, 6.49)
+peanut_butter = Item("no name peanut butter", "Club Size", 8.00, 6.99)
+condensed_milk = Item("condensed milk", "Brand Sweetened Condensed Milk", 3.00, 2.49)
+chicken_korma = Item("chicken korma", "President's Choice", 4.00, 3.49)
+annies_crackers = Item("annies crackers", "White Cheddar", 4.00, 3.49)
 
-superstore_foods = [lentils, peas, oats, peanut_butter, condensed_milk, chicken_korma, annies_crackers]
+superstore_items = [lentils, peas, oats, peanut_butter, condensed_milk, chicken_korma, annies_crackers]
 
 # randomizing array to randomize search order (human behaviour)
-superstore_foods_randomized = []
-for i in range(len(superstore_foods)):
-    rand_i = random.randint(0, len(superstore_foods)-1)
-    superstore_foods_randomized.append(superstore_foods[rand_i])
-    superstore_foods.pop(rand_i)
+superstore_items_randomized = []
+for i in range(len(superstore_items)):
+    rand_i = random.randint(0, len(superstore_items)-1)
+    superstore_items_randomized.append(superstore_items[rand_i])
+    superstore_items.pop(rand_i)
     
 # Stalls program execution by 3-7 seconds to simulate human web-scrolling behaviour.  
 def stall():
@@ -90,15 +90,19 @@ driver.implicitly_wait(5) # wait max 5 seconds before executing commands with th
 driver.get(new_tab)
 driver.maximize_window() # human behaviour
 
+
+
+######################### SUPERSTORE ################################
+
 driver.get(superstore_url)
 stall()
 
+for i in range(len(superstore_items_randomized)):
     # close modals: not necessary for Superstore
     # close_modal_button = driver.find_element(By.ID, "closeActivityPop").click()
 
-for i in range(len(superstore_foods_randomized)):
     # engage with search bar
-    search = superstore_foods_randomized[i].search
+    search = superstore_items_randomized[i].search
     search_bar = driver.find_element(By.CLASS_NAME, "search-input__input") 
     search_bar.click()
     search_bar.clear()
@@ -107,7 +111,7 @@ for i in range(len(superstore_foods_randomized)):
     search_bar.send_keys(Keys.RETURN)
 
     # find the div of the desired item from search results
-    desired_item = superstore_foods_randomized[i].desired_item
+    desired_item = superstore_items_randomized[i].desired_item
     search_results = driver.find_elements(By.TAG_NAME, "h3")
 
     for item in search_results:
@@ -131,16 +135,93 @@ for i in range(len(superstore_foods_randomized)):
             price = float ( format( ( float(price)/float(price_array[0]) ), ".2f" ) ) # formatting necessary due to price sometimes being 1 decimal place
 
         # compare the price to the max_buy_price
-        if (price <= superstore_foods_randomized[i].max_buy_price):
-            print("{item} on sale! The price is: {price} vs the cheapest price: {cheapest_price}.".format(item = superstore_foods_randomized[i].search.capitalize(), price = price, cheapest_price = superstore_foods_randomized[i].cheapest_price))
+        if (price <= superstore_items_randomized[i].max_buy_price):
+            print("{item} on sale! The price is: {price} vs the cheapest price: {cheapest_price}.".format(item = superstore_items_randomized[i].search.capitalize(), price = price, cheapest_price = superstore_items_randomized[i].cheapest_price))
         else:
-            print("This food is not on sale: " + superstore_foods_randomized[i].search)
+            print("This item is not on sale: " + superstore_items_randomized[i].search)
             print("Moving on to the next item...")
             continue
     else:    
-        print("This food is not on sale: " + superstore_foods_randomized[i].search)
+        print("This item is not on sale: " + superstore_items_randomized[i].search)
         print("Moving on to the next item...")
         continue
+
+stall()
+
+
+######################### NO FRILLS ################################
+
+# instantiating No Frills items
+
+lentils = Item("red split lentils", "1.8 kg", 4.50, 3.99)
+peas = Item("no name green peas", "Club Size", 5.00, 4.49)
+peanut_butter = Item("no name peanut butter", "Club Size", 7.99, 6.99)
+condensed_milk = Item("condensed milk", "Brand Sweetened Condensed Milk", 3.00, 2.49)
+
+nofrills_items = [lentils, peas, peanut_butter, condensed_milk]
+
+# randomizing array to randomize search order (human behaviour)
+nofrills_items_randomized = []
+for i in range(len(nofrills_items)):
+    rand_i = random.randint(0, len(nofrills_items)-1)
+    nofrills_items_randomized.append(nofrills_items[rand_i])
+    nofrills_items.pop(rand_i)
+
+nofrills_url = "https://www.nofrills.ca/"
+driver.get(nofrills_url)
+stall()
+
+for i in range(len(nofrills_items_randomized)):
+    # close modals: not necessary for No Frills
+    # close_modal_button = driver.find_element(By.ID, "closeActivityPop").click()
+
+    # engage with search bar
+    search = nofrills_items_randomized[i].search
+    search_bar = driver.find_element(By.CLASS_NAME, "search-input__input") 
+    search_bar.click()
+    search_bar.clear()
+    search_bar.send_keys(search)
+    stall()
+    search_bar.send_keys(Keys.RETURN)
+
+    # find the div of the desired item from search results
+    desired_item = nofrills_items_randomized[i].desired_item
+    search_results = driver.find_elements(By.TAG_NAME, "h3")
+
+    for item in search_results:
+        if desired_item in item.text:
+            desired_item_div = item
+
+    # check if the desired item is on sale 
+    # note: we assume a No Frills item is on sale IFF its badge div contains text
+    badge_div = desired_item_div.find_element(By.XPATH, "./following-sibling::div/div[1]")
+    price_div = desired_item_div.find_element(By.XPATH, "./following-sibling::div/div[2]")
+    sale_price_div = desired_item_div.find_element(By.XPATH, "./following-sibling::div/div[3]")
+
+    if (badge_div.text): # IFF
+        # get the price
+        if ("LIMIT" in badge_div.text): # limit format: "$4.29 LIMIT 4"
+            price_array = price_div.text.split()
+            price = price_array[0]
+            price = float((price.replace("$", "")))
+        elif ("MULTI" in badge_div.text): # multi format: "$4.50 MIN 2"
+            price_array = price_div.text.split()
+            price = price_array[0]
+            price = float( price.replace("$", "").format(".2f") ) # formatting necessary due to price sometimes being 1 decimal place
+        elif ("SALE" in badge_div.text): # sale format: "$4.29ea$5.99ea $0.24/ 100g"
+            price_array = sale_price_div.text.split("ea")
+            price = price_array[0]
+            price = float(price.replace("$", ""))
+
+        # compare the price to the max_buy_price
+        if (price <= nofrills_items_randomized[i].max_buy_price):
+            print("{item} on sale! The price is: {price} vs the cheapest price: {cheapest_price}.".format(item = nofrills_items_randomized[i].search.capitalize(), price = price, cheapest_price = nofrills_items_randomized[i].cheapest_price))
+            continue
+        else:
+            pass
+    print("This item is not on sale: " + nofrills_items_randomized[i].search)
+    print("Moving on to the next item...")
+    continue
 
 stall()
 driver.quit()
