44e8b6f138d62f1d4b7d5d34604f6427bddbf494
Louie <yi-jie.lu@hotmail.com>
2022-09-23 09:51:30 -0700

    Add Food class
    Can successfully iterate through an array of (Superstore) Food objects

--- grocery_prices2.py
+++ grocery_prices2.py
@@ -45,11 +45,41 @@ driver = webdriver.Chrome(PATH)
 # rp.read()
 # print(rp.can_fetch("*", "https://www.tntsupermarket.com/eng/75551501-tnt-thai-jasmine-rice-40lb.html"))
 
-new_tab = "chrome://newtab"
 tnt_url = "https://www.tntsupermarket.com/eng/75551501-tnt-thai-jasmine-rice-40lb.html" # T&T is being weird, searching
 # "t&t thai jasmine rice" won't return any results on the headless browser. additionally, visiting the link directly will 
 # show a difference price ($43.xx) than the price on a normal browser ($46.19)
+
+
+
+new_tab = "chrome://newtab"
 superstore_url = "https://www.realcanadiansuperstore.ca/"
+
+
+# instantiating Superstore foods
+class Food:
+    def __init__(self, search, desired_item, max_buy_price, cheapest_price):
+        self.search = search
+        self.desired_item = desired_item
+        self.max_buy_price = max_buy_price
+        self.cheapest_price = cheapest_price
+
+lentils = Food("red split lentils", "1.8 kg", 4.49, 3.99)
+peas = Food("no name green peas", "Club Size", 4.99, 4.49)
+oats = Food("quick oats", "5.16 kg", 8.99, 6.49)
+peanut_butter = Food("no name peanut butter", "Club Size", 7.99, 6.99)
+condensed_milk = Food("condensed milk", "Brand Sweetened Condensed Milk", 2.99, 2.49)
+
+superstore_foods = [lentils, peas, oats, peanut_butter, condensed_milk]
+
+# randomizing array to randomize search order (human behaviour)
+superstore_foods_randomized = []
+for i in range(len(superstore_foods)):
+    rand_i = random.randint(0, len(superstore_foods)-1)
+    superstore_foods_randomized.append(superstore_foods[rand_i])
+    superstore_foods.pop(rand_i)
+
+
+
     
 # Stalls program execution by 3-7 seconds to simulate human web-scrolling behaviour.  
 def stall():
@@ -64,40 +94,43 @@ driver.maximize_window() # human behaviour
 driver.get(superstore_url)
 stall()
 
-# close modals: not necessary for Superstore
-# close_modal_button = driver.find_element(By.ID, "closeActivityPop").click()
+    # close modals: not necessary for Superstore
+    # close_modal_button = driver.find_element(By.ID, "closeActivityPop").click()
 
-# engage with search bar
-search = "red split lentils"
+for i in range(len(superstore_foods_randomized)):
+    # engage with search bar
+    search = superstore_foods_randomized[i].search
 
-search_bar = driver.find_element(By.CLASS_NAME, "search-input__input") 
-search_bar.click()
-search_bar.send_keys(search)
-stall()
-search_bar.send_keys(Keys.RETURN)
+    search_bar = driver.find_element(By.CLASS_NAME, "search-input__input") 
+    search_bar.click()
+    search_bar.clear()
+    search_bar.send_keys(search)
+    stall()
+    search_bar.send_keys(Keys.RETURN)
 
-# find desired item from search results
-desired_item = "1.8 kg"
-search_results = driver.find_elements(By.TAG_NAME, "h3")
+    # find the div of the desired item from search results
+    desired_item = superstore_foods_randomized[i].desired_item
+    
+    search_results = driver.find_elements(By.TAG_NAME, "h3")
 
-for item in search_results:
-    if desired_item in item.text:
-        desired_item_div = item
+    for item in search_results:
+        if desired_item in item.text:
+            desired_item_div = item
 
-# check if the desired item is on sale 
-# note: we assume a Superstore item is on sale IFF it has a limit div.
-try: 
+    # check if the desired item is on sale 
+    # note: we assume a Superstore item is on sale IFF its limit div contains text
     limit_div = desired_item_div.find_element(By.XPATH, "./following-sibling::div/div[2]")
-except:
-    print("No sale. Moving on to the next item...")
-
-# get the price. example of price_div.text: "$4.29 LIMIT 4"
-price = limit_div.text.split()
-price = price[0]
-price = price.replace("$", "")
-print(price)
+    if (limit_div.text): # IFF
+        # get the price. example of limit_div.text: "$4.29 LIMIT 4"
+        price = limit_div.text.split()
+        price = price[0]
+        price = price.replace("$", "")
+        print("This food is on sale: " + superstore_foods_randomized[i].search)
+        print("The price is: " + price)
+    else:    
+        print("This food is not on sale: " + superstore_foods_randomized[i].search)
+        print("No LIMIT. Moving on to the next item...")
+        continue
 
 stall()
-
 driver.quit()
-quit()
