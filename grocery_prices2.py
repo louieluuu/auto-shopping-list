1968e6963d8836d88141af9f55154bf4da4354aa
Louie <yi-jie.lu@hotmail.com>
2022-09-25 10:03:08 -0700

    Add ability to write to Shopping List and update Database (Superstore)

--- grocery_prices2.py
+++ grocery_prices2.py
@@ -1,5 +1,6 @@
 # for testing
 from ast import Return
+from asyncio.windows_events import NULL
 from cgitb import text
 from tkinter import SCROLL
 import unittest 
@@ -38,6 +39,8 @@ def stall():
 scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis.com/auth/drive"]
 credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(credentials)
+database = client.open("Grocery Database")
+superstore_worksheet = database.worksheet("Superstore")
 
 # setting up webdriver
 PATH = "C:\Program Files (x86)\chromedriver.exe"
@@ -47,6 +50,7 @@ driver.implicitly_wait(5) # if element is not found on the page due to loading,
 
 new_tab = "chrome://newtab"
 superstore_url = "https://www.realcanadiansuperstore.ca/"
+shopping_list = []
 
 # Step 1: Import the Grocery Database from Google Sheets into a CSV list, superstore_items.
 # each element in superstore_items contains the following values:
@@ -55,14 +59,12 @@ superstore_url = "https://www.realcanadiansuperstore.ca/"
 # element[2] = max_buy_price
 # element[3] = cheapest_price
 
-sheet = client.open("Grocery Database")
-superstore_items = sheet.worksheet("Superstore").get_values("B2:E")
-random.shuffle(superstore_items) # human behaviour: randomize search order
-
 driver.get(new_tab)
 driver.maximize_window() # human behaviour
 
 ######################### SUPERSTORE ################################
+superstore_items = superstore_worksheet.get_values("B2:E")
+random.shuffle(superstore_items) # human behaviour: randomize search order
 
 driver.get(superstore_url)
 stall()
@@ -115,21 +117,59 @@ for item in superstore_items:
             price = price_array[2]
             price = price.replace("$", "")
             price = float ( format( ( float(price)/float(price_array[0]) ), ".2f" ) ) # formatting necessary due to price sometimes being 1 decimal place
-
         stall()
 
         # compare the price to the max_buy_price
         if (price <= max_buy_price):
-            # print("{item} on sale! The price is: {price} vs the cheapest price: {cheapest_price}.".format(item = search.capitalize(), price = price, cheapest_price = cheapest_price))
             print("{item} is on sale! Adding to the Shopping List...".format(item = search.capitalize()))
+            item.append(price) # saving the price so we can access it later
+        # also, if the price is the cheapest yet seen...
+            if (price < cheapest_price):
+                # update the database
+                item_cell = superstore_worksheet.find(search)
+                cheapest_price_cell = "E" + str(item_cell.row)
+                superstore_worksheet.update(cheapest_price_cell, price)
+                # update the item
+                item[3] = price
+            shopping_list.append(item)
             continue
         else:
             pass
     print("This item is not on sale: " + search + ". Moving on to the next item...")
 
+# SHOPPING LIST LOGIC #
+try: 
+    shopping_sheet = client.open("Shopping List (Louie)").sheet1
+    shopping_sheet.clear()
+except gspread.exceptions.SpreadsheetNotFound:
+    shopping_sheet = client.create("Shopping List (Louie)")
+    shopping_sheet.share("goldjet32@gmail.com", perm_type="user", role="writer")
+    shopping_sheet = client.open("Shopping List (Louie)").sheet1
+
+shopping_sheet.update("A1:D1", [["Item", "Regular Price", "Current Price", "Cheapest Price"]])
+
+# each element in shopping_list is an item which contains:
+# element[0] = search
+# element[1] = desired_item
+# element[2] = max_buy_price
+# element[3] = cheapest_price
+# element[4] = current_price
+
+for item in shopping_list:
+    item_name = item[0]
+    regular_price = "" # for now
+    current_price = item[4]
+    cheapest_price = item[3]
+    new_row = [item_name, regular_price, current_price, cheapest_price]
+    shopping_sheet.append_row(new_row)
+
+
+driver.quit()
+quit()
+
 ######################### NO FRILLS ################################
 
-nofrills_items = sheet.worksheet("No Frills").get_values("B2:E")
+nofrills_items = database.worksheet("No Frills").get_values("B2:E")
 random.shuffle(nofrills_items)
 
 nofrills_url = "https://www.nofrills.ca/"
