54fbb7cbac8c74d66ead57e3da1d56735397472b
Louie <yi-jie.lu@hotmail.com>
2022-10-06 16:53:36 -0700

    Modularize program pt. 2
    -added main function
    -generalized all prev instances of "Superstore" to the generic "store"
    -added is_sale, parse_price funcs
    -next step: simplify scrape(), and refactor code order

--- main.py
+++ main.py
@@ -32,8 +32,8 @@ from oauth2client.service_account import ServiceAccountCredentials
 # for handling data
 import pandas as pd
 
-# importing stores
-# from stores import Superstore
+# for getattr()
+from sys import modules
 
 # human behaviour: stall program execution to simulate web-surfing  
 def stall():
@@ -44,7 +44,6 @@ scope = ['https://www.googleapis.com/auth/spreadsheets', "https://www.googleapis
 credentials = ServiceAccountCredentials.from_json_keyfile_name("credentials.json", scope)
 client = gspread.authorize(credentials)
 database = client.open("Grocery Database")
-store = database.worksheet("Superstore")
 database_range = "A2:G"
 
 # setting up webdriver
@@ -55,6 +54,83 @@ driver.implicitly_wait(5) # if element is not found on the page due to loading,
 
 new_tab = "chrome://newtab"
 shopping_list = []
+stores = []
+for worksheet in database.worksheets():
+    stores.append(worksheet.title)
+
+############### CLASS SUPERSTORE ####################
+
+class Superstore():
+    url = "https://www.realcanadiansuperstore.ca/"
+    def search_bar():
+        return driver.find_element(By.CLASS_NAME, "search-input__input")
+
+    def search_results():
+        return driver.find_elements(By.CLASS_NAME, "product-tile__details__info")
+
+    def is_sale(filtered_item):
+        # checks for presence of a badge
+        try:    
+            filtered_item.find_element(By.CSS_SELECTOR, ".product-tile-deal-badge")
+            return True
+        except NoSuchElementException:
+            return False
+
+    def parse_price(filtered_item):
+        price = filtered_item.find_element(By.CSS_SELECTOR, ".product-badge__text.product-badge__text--product-tile").text
+        if ("LIMIT" in price):
+            # ex. format: $4.29 LIMIT 4
+            price_array = price.split()
+            price = float(price_array[0].replace("$", ""))
+        elif ("FOR" in price):
+            # ex. format: 2 FOR $9.00
+            price_array = price.split()
+            divisor = float(price_array[0])
+            price = float(price_array[2].replace("$", ""))
+            price = float(f"{(price/divisor):.2f}")
+        return price
+
+    def regular_price(filtered_item):
+        regular_price = filtered_item.find_element(By.CSS_SELECTOR, ".selling-price-list.selling-price-list--product-tile").text
+        return regular_price
+
+############ SHOPPING LIST LOGIC ##############
+def create_list():
+    try: 
+        shopping_sheet = client.open("Shopping List (Louie)").sheet1
+        shopping_sheet.clear()
+    except gspread.exceptions.SpreadsheetNotFound:
+        shopping_sheet = client.create("Shopping List (Louie)")
+        shopping_sheet.share("goldjet32@gmail.com", perm_type="user", role="writer")
+        shopping_sheet = client.open("Shopping List (Louie)").sheet1
+
+    shopping_titles = "A1:E1"
+    shopping_sheet.update(shopping_titles, [["URL", "Item", "Regular Price", "Current Price", "Cheapest Price"]])
+
+    # each item contains the following values:
+    # item[0] = URL
+    # item[1] = item
+    # item[2] = search
+    # item[3] = search_filter
+    # item[4] = regular_price
+    # item[5] = max_buy_price
+    # item[6] = cheapest_price
+    # item[7] = current_price (appended earlier)
+
+    # append the sale items
+
+    for item in shopping_list:
+        url = item[0]
+        item_name = item[1]
+        regular_price = item[4]
+        current_price = item[7]
+        cheapest_price = item[6]
+        new_row = [url, item_name, regular_price, current_price, cheapest_price]
+        shopping_sheet.append_row(new_row)
+
+    # format the spreadsheet
+    shopping_sheet.columns_auto_resize(1, 4) # exclude the URL to keep it small
+    shopping_sheet.format([shopping_titles], {"textFormat": {"fontSize": 10, "bold": True}})
 
 # Step 1: Import the Grocery Database from Google Sheets into a CSV list, superstore_items.
 # each item in superstore_items contains the following values:
@@ -66,181 +142,115 @@ shopping_list = []
 # item[5] = max_buy_price
 # item[6] = cheapest_price
 
-class Superstore():
-    url = "https://www.realcanadiansuperstore.ca/"
-    def search_bar():
-        search_bar = driver.find_element(By.CLASS_NAME, "search-input__input")
-        return search_bar
+def scrape(store: str):
+    worksheet = database.worksheet(store)
+    items = worksheet.get_values(database_range)
+    random.shuffle(items) # human behaviour
 
-    def search_results():
-        search_results = driver.find_elements(By.CLASS_NAME, "product-tile__details__info")
-        return search_results
+    # Get the Class associated with the string
+    store = getattr(modules[__name__], store)
 
-    def badge():
-        badge = filtered_item.find_element(By.CSS_SELECTOR, ".product-badge__icon.product-badge__icon--limit.product-badge__icon--product-tile")
-        return badge
-
-    def sale_price():
-        try:    
-            sale_price = filtered_item.find_element(By.CSS_SELECTOR, ".product-badge__text.product-badge__text--product-tile").text
-        except NoSuchElementException:
-            sale_price = ""
-        return sale_price
-
-    def regular_price():
-        regular_price = filtered_item.find_element(By.CSS_SELECTOR, ".selling-price-list.selling-price-list--product-tile").text
-        return regular_price
-
-
-
-
-######################### SUPERSTORE ################################
-
-items = store.get_values(database_range)
-random.shuffle(items) # human behaviour: randomize search order
-
-driver.get(new_tab)
-driver.maximize_window() # human behaviour
-driver.get(Superstore.url)
-stall()
-
-for item in items:
-    # close modals:
-    #
-
-    # define variables, as specified in Step 1
-    item_url = item[0]
-    item_name = item[1]
-    search = item[2]
-    search_filter = item[3]
-    regular_price = item[4]
-    max_buy_price = float(item[5])
-
-    # this try except block is necessary to cover some strange behaviour that occurs when
-    # all initial cheapest_price (last column) fields are set to nothing.
-    try:
-        cheapest_price = float(item[6])
-    except IndexError:
-        item.append("")
-        cheapest_price = ""
-    except ValueError:
-        cheapest_price = ""
-
-    # Step 1: Engage with search bar
-    search_bar = Superstore.search_bar()
-    search_bar.click()
-    search_bar.clear()
-    search_bar.send_keys(search)
+    driver.get(new_tab)
+    driver.maximize_window() # human behaviour
+    driver.get(store.url)
     stall()
-    search_bar.send_keys(Keys.RETURN)
 
-    # Step 2: Filter desired item from search results
-    # combing through too many search results may match an unrelated item that happens to share the search_filter.
-    # therefore, introduce a search cap.
-    filtered_item = None
-    search_results = Superstore.search_results()
-    search_cap = 8
-    i = 0
-    while i < min(len(search_results), search_cap):
-        if search_filter in search_results[i].text: 
-            filtered_item = search_results[i]
-            break
-        else:
-            i += 1
+    for item in items:
+        # close modals:
+        #
 
-    if (filtered_item == None):
-        print(f"{search.upper()} not found in the search results. Moving on to the next item...")
+        # define variables, as specified in Step 1
+        item_url = item[0]
+        item_name = item[1]
+        search = item[2]
+        search_filter = item[3]
+        regular_price = item[4]
+        max_buy_price = float(item[5])
+
+        # this try except block is necessary to cover some strange behaviour that occurs when
+        # all initial cheapest_price (last column) fields are set to nothing.
+        try:
+            cheapest_price = float(item[6])
+        except IndexError:
+            item.append("")
+            cheapest_price = ""
+        except ValueError:
+            cheapest_price = ""
+
+        # Step 1: Engage with search bar
+        search_bar = store.search_bar()
+        search_bar.click()
+        search_bar.clear()
+        search_bar.send_keys(search)
         stall()
-        continue
+        search_bar.send_keys(Keys.RETURN)
 
-    # Step 3: Determine if the filtered item is on sale.
-    price = Superstore.sale_price()
-    if ("LIMIT" in price):
-        # ex. format: $4.29 LIMIT 4
-        price_array = price.split()
-        price = float(price_array[0].replace("$", ""))
+        # Step 2: Filter desired item from search results
+        # combing through too many search results may match an unrelated item that happens to share the search_filter.
+        # therefore, introduce a search cap.
+        filtered_item = None
+        search_results = store.search_results()
+        search_cap = 8
+        i = 0
+        while i < min(len(search_results), search_cap):
+            if search_filter in search_results[i].text: 
+                filtered_item = search_results[i]
+                break
+            else:
+                i += 1
 
-    elif ("FOR" in price):
-        # ex. format: 2 FOR $9.00
-        price_array = price.split()
-        divisor = float(price_array[0])
-        price = float(price_array[2].replace("$", ""))
-        price = float(f"{(price/divisor):.2f}")
+        if (filtered_item == None):
+            print(f"{search.upper()} not found in the search results. Moving on to the next item...")
+            stall()
+            continue
 
-    elif (price == ""): # item is not on sale: update the regular price
-        regular_price = Superstore.regular_price()
-        # ex. formats: $6.69ea, $11.49c01
-        regular_price = regular_price.replace("$", "")
-        regular_price = float(regular_price[0:regular_price.index(".")+3])
+        # Step 3: Determine if the filtered item is on sale.
+        if store.is_sale(filtered_item):
+            price = store.parse_price(filtered_item)
 
-        # update the database
-        item_cell = store.find(search)
-        regular_price_cell = "E" + str(item_cell.row)
-        store.update(regular_price_cell, regular_price)
+        else: # item is not on sale: update regular price
+            regular_price = store.regular_price(filtered_item)
+            # ex. formats: $6.69ea, $11.49c01
+            regular_price = regular_price.replace("$", "")
+            regular_price = float(regular_price[0:regular_price.index(".")+3])
 
-        # update the item
-        item[4] = regular_price
+            # update the database
+            item_cell = worksheet.find(search)
+            regular_price_cell = "E" + str(item_cell.row)
+            worksheet.update(regular_price_cell, regular_price)
 
-        print(f"{search.upper()} is not on sale. Moving on to the next item...")
-        continue
+            # update the item
+            item[4] = regular_price
 
-    # Step 4: Compare the price to the max_buy_price.
-    if (price <= max_buy_price):
-        print(f"{search.upper()} is on sale! Adding to the Shopping List...")
-        item.append(price) # saving the price so we can access it later as current_price
+            print(f"{search.upper()} is not on sale. Moving on to the next item...")
+            continue
+
+        # Step 4: Compare the price to the max_buy_price.
+        if (price <= max_buy_price):
+            print(f"{search.upper()} is on sale! Adding to the Shopping List...")
+            item.append(price) # saving the price so we can access it later as current_price
 
         # if the cheapest price wasn't inputted, OR price is the cheapest yet seen...
-        if (cheapest_price == "") or (price < cheapest_price):
-            # update the database
-            item_cell = store.find(search)
-            cheapest_price_cell = "G" + str(item_cell.row)
-            store.update(cheapest_price_cell, price)
-            # update the item
-            item[6] = price
+            if (cheapest_price == "") or (price < cheapest_price):
+                # update the database
+                item_cell = worksheet.find(search)
+                cheapest_price_cell = "G" + str(item_cell.row)
+                worksheet.update(cheapest_price_cell, price)
+                # update the item
+                item[6] = price
 
-        shopping_list.append(item)
+            shopping_list.append(item)
 
-    elif (price > max_buy_price):
-        print(f"{search.upper()} is on sale, but still too expensive to buy. Moving on to the next item...")
-        continue
+        elif (price > max_buy_price):
+            print(f"{search.upper()} is on sale, but still too expensive to buy. Moving on to the next item...")
+            continue
+        stall()
 
-    stall()
 
-# SHOPPING LIST LOGIC #
-try: 
-    shopping_sheet = client.open("Shopping List (Louie)").sheet1
-    shopping_sheet.clear()
-except gspread.exceptions.SpreadsheetNotFound:
-    shopping_sheet = client.create("Shopping List (Louie)")
-    shopping_sheet.share("goldjet32@gmail.com", perm_type="user", role="writer")
-    shopping_sheet = client.open("Shopping List (Louie)").sheet1
-
-shopping_titles = "A1:E1"
-shopping_sheet.update(shopping_titles, [["URL", "Item", "Regular Price", "Current Price", "Cheapest Price"]])
-
-# each item contains the following values:
-# item[0] = URL
-# item[1] = item
-# item[2] = search
-# item[3] = search_filter
-# item[4] = regular_price
-# item[5] = max_buy_price
-# item[6] = cheapest_price
-# item[7] = current_price (appended earlier)
-
-# append the sale items
-for item in shopping_list:
-    url = item[0]
-    item_name = item[1]
-    regular_price = item[4]
-    current_price = item[7]
-    cheapest_price = item[6]
-    new_row = [url, item_name, regular_price, current_price, cheapest_price]
-    shopping_sheet.append_row(new_row)
-
-# format the spreadsheet
-shopping_sheet.columns_auto_resize(1, 4) # exclude the URL to keep it small
-shopping_sheet.format([shopping_titles], {"textFormat": {"fontSize": 10, "bold": True}})
+############# MAIN #################
+for store in stores:
+    scrape(store) #this will be multiprocessed
+    create_list()
 
 driver.quit()
 quit()
